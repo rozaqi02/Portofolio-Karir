b4dca2d785035d545dd91411e6c91e919eaea93a
rozaqi02 <rojaki1419@gmail.com>
2025-05-17 16:45:05 +0700

    penyempurnaan #2

--- script.js
+++ script.js
@@ -31,26 +31,31 @@ projectCards.forEach(card => {
 const modal = document.getElementById('project-modal');
 const modalTitle = document.getElementById('modal-title');
 const modalDescription = document.getElementById('modal-description');
-const modalImage = document.getElementById('modal-image');
+const modalImages = document.getElementById('modal-images');
 const modalGithub = document.getElementById('modal-github');
+const modalDetails = document.getElementById('modal-details');
 const closeModal = document.querySelector('.close-modal');
 
 const projectDetails = {
     'survey-polinema': {
         title: 'Sistem Survei Kepuasan Pelanggan Polinema',
         description: 'Mengembangkan aplikasi web untuk mengelola survei kepuasan pemangku kepentingan Politeknik Negeri Malang guna meningkatkan kualitas layanan. Sistem ini mencakup pengumpulan jawaban responden dan pembuatan laporan hasil survei untuk analisis lebih lanjut. Proyek ini menggunakan PHP, HTML, CSS, JavaScript, MySQL, dan template UI AdminLTE.',
-        image: 'images/survey-polinema-screenshot.png',
-        github: 'https://github.com/rozaqi02/SISTEM-SURVEY-KEPUASAN-PELANGGAN-POLINEMA'
+        images: ['images/survey-polinema-screenshot.png'],
+        github: 'https://github.com/rozaqi02/SISTEM-SURVEY-KEPUASAN-PELANGGAN-POLINEMA',
+        details: null
     },
     'jti-simpen': {
         title: 'JTI-SIMPEN (Sistem Informasi Kompensasi)',
         description: 'Membangun sistem berbasis web dan mobile untuk pengelolaan kompensasi mahasiswa, meliputi pengajuan, pemilihan pekerjaan, validasi, dan pelacakan status secara real-time. Sistem ini mempermudah pemantauan kompensasi dengan pengalaman pengguna yang efisien, menggunakan Laravel, MySQL, API development, dan desain UI/UX.',
-        image: 'images/jti-simpen-screenshot.png',
-        image: 'images/jti-simpen-screenshot2.png',
-        image: 'images/jti-simpen-screenshot3.png',
-        image: 'images/jti-simpen-screenshot4.png',
-        image: 'images/jti-simpen-screenshot5.png',
-        github: 'https://github.com/rozaqi02/JTI_SIMPEN'
+        images: [
+            'images/jti-simpen-screenshot.png',
+            'images/jti-simpen-screenshot2.png',
+            'images/jti-simpen-screenshot3.png',
+            'images/jti-simpen-screenshot4.png',
+            'images/jti-simpen-screenshot5.png'
+        ],
+        github: 'https://github.com/rozaqi02/JTI_SIMPEN',
+        details: 'https://drive.google.com/drive/folders/1mh5cY1wzRXdRaft6CnQlma87uXVkF_Gi?usp=drive_link'
     }
 };
 
@@ -60,8 +65,27 @@ projectCards.forEach(card => {
         const project = projectDetails[projectId];
         modalTitle.textContent = project.title;
         modalDescription.textContent = project.description;
-        modalImage.src = project.image;
+        
+        // Clear previous images and add new ones
+        modalImages.innerHTML = '';
+        project.images.forEach(src => {
+            const img = document.createElement('img');
+            img.src = src;
+            img.alt = 'Project Screenshot';
+            img.classList.add('modal-image');
+            modalImages.appendChild(img);
+        });
+
         modalGithub.href = project.github;
+
+        // Show or hide the "Lihat Detailnya" link based on project
+        if (project.details) {
+            modalDetails.href = project.details;
+            modalDetails.style.display = 'inline-block';
+        } else {
+            modalDetails.style.display = 'none';
+        }
+
         modal.style.display = 'flex';
     });
 });
